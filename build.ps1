9a109a04e5dda69f1d7e3bd266363846576b486d
pepsakdoek <65312601+pepsakdoek@users.noreply.github.com>
2026-02-05 09:29:18 +0200

    rename files and prepare for better bucket name

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 # 1. Delete the existing bundle if it exists
-if (Test-Path "./viz.js") { Remove-Item "./viz.js" }
+if (Test-Path "./qlikpickerviz.js") { Remove-Item "./qlikpickerviz.js" }
 
 # 2. Concatenate files in order
 # We use -Raw to ensure no extra newlines are injected between files
@@ -8,6 +8,6 @@ $files = @(
     "./main.js"
 )
 
-Get-Content $files -Raw | Out-File -FilePath "./viz.js" -Encoding utf8
+Get-Content $files -Raw | Out-File -FilePath "./qlikpickerviz.js" -Encoding utf8
 
-Write-Host "Build Complete: viz.js created in root." -ForegroundColor Green
+Write-Host "Build Complete: qlikpickerviz.js created in root." -ForegroundColor Green
